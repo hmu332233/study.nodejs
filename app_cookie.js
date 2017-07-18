89c4617cff5f995231a673b9165f6b540601b04d
minung han <hmu332233@gmail.com>
2017-07-18 23:17:29 +0900

    [수정] app_cookie.js - 쿠기 암호화 추가

--- app_cookie.js
+++ app_cookie.js
@@ -1,19 +1,19 @@
 var express = require("express");
 var app = express();
-//쿠기를 사용하기 위한 라이브러리
-var cookieParser = require('cookie-parser');
+//쿠기를 사용하기 위한 라이브러리(두번째 인자는 암호화 key)
+var cookieParser = require('cookie-parser','123456');
 
 app.use(cookieParser());
 
 app.get('/count',function(req,res){
     
-    if(req.cookies.count)
-      var count = parseInt(req.cookies.count);
+    if(req.signedcookies.count)
+      var count = parseInt(req.signedcookies.count);
     else
       var count = 0;
     
     
-    res.cookie('count', count+1);
+    res.cookie('count', count+1,{signed:true});
     
     res.send('count :' + count ); 
 });
