86b6c96eef957ac640012ccd4237cf42559aca5c
minung han <hmu332233@gmail.com>
2017-07-27 03:17:05 +0900

    [수정] route 분리

--- projects/opentutorials/app.js
+++ projects/opentutorials/app.js
@@ -51,9 +51,6 @@ app.use(passport.session());
 
 
 
-app.get('/auth/login', function (req, res){
-  res.render('auth/login.html.ejs');
-})
 
 
 //여기서 정의한 함수가 사용자 인증에 사용된다
@@ -151,72 +148,6 @@ passport.deserializeUser(function(id, done) {
 });
 
 
-app.post( '/auth/login',  
-          passport.authenticate(
-            //local 이라는 인증 방식이 사용됨 만약 facebook을 사용중이면 facebook을 입력하면된다.
-            'local', 
-            { 
-              //로그인에 성공하면 redirect 되는 url
-              successRedirect: '/welcome',
-              //로그인에 실패하면 redirect 되는 url
-              failureRedirect: '/auth/login',
-              //실패했을 시, 메세지를 띄우기 위함
-              failureFlash: false 
-            }
-          )
-);
-
-app.get( '/auth/facebook',
-          passport.authenticate('facebook',{ scope: 'email' })
-);
-          
-app.get( '/auth/facebook/callback',
-          passport.authenticate(
-            'facebook',
-            { 
-              successRedirect: '/welcome',
-              failureRedirect: '/auth/login' 
-            }
-          )
-);
-          
-
-app.get('/auth/register', function (req, res){
-  res.render('auth/register.html.ejs');
-})
-
-app.post('/auth/register', function (req, res,next){
-  
-  var opts = {
-    password: req.body.password
-  }
-  
-  hasher(opts, function(err, pass, _salt, hash) {
-    var user = {
-      authId: 'local:'+req.body.username,
-      username: req.body.username,
-      password: hash,
-      displayName: req.body.displayName,
-      salt: _salt
-    }
-    
-    
-    //sql문을 일일이 작성할 필요없이 다음과 같이 객체만 넣으면 자동으로 넣어준다.
-    var sql = 'INSERT INTO users SET ?'
-    connection.query(sql, user, function(err, results){
-        if(err){
-            console.log(err);
-            res.status(500);
-        } else {
-            req.login(user,function(err){
-            if(err) return next(err);
-                return res.redirect('/welcome')
-            });
-        }
-    });
-    
-    })
-})
 
 app.get('/welcome', function (req, res) {
   
@@ -233,13 +164,9 @@ app.get('/welcome', function (req, res) {
     `)
 })
 
-app.get('/auth/logout', function (req, res){
-  req.logout();
-  req.session.save(function(){
-    res.redirect('/welcome');  
-  });
-})
-
+//route
+var auth = require('./routes/auth')(passport);
+app.use('/auth', auth);
 
 app.listen(process.env.PORT, function () {
   console.log('Connected!')
