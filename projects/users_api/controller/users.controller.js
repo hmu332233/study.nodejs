26964fc9c8172ee5084e6a5cb5086ac316820d65
minung han <hmu332233@gmail.com>
2017-08-03 21:31:13 +0900

    [추가] create, find

--- projects/users_api/controller/users.controller.js
+++ projects/users_api/controller/users.controller.js
@@ -1,17 +1,47 @@
+var User = require("../models/user");
+
 exports.create = function (req, res) {
-    
+  var user = new User(req.body);
+  
+  user.save(function (err, doc){
+    if(err) return res.status(500).send(err);
+    res.send("User saved successfullly:\n" + doc);
+  });
 };
 
 exports.findAll = function (req, res) {
+  
+  User.find({}, function(err, users){
+    if(err) return res.status(500).send(err);
+    if(!users.length) return res.status(404).send({err: "User not found"});
+    res.send("User find successfully:\n" + users);
+  });
     
 };
 
 exports.findOne = function (req, res) {
+  
+  var id = req.params.id;
+  console.log(id);
+  
+  User.findById(id, function (err, user) {
+    if(err) return res.status(500).send(err);
+    if(!user) return res.status(404).send({ err: "User not found"});
+    res.send("User find successfully:\n" + user);
+  });
     
 };
 
 exports.findByName = function (req, res) {
-    
+  var username = req.params.username;
+  console.log(username);
+  
+  User.findOne({ "name": username }, function (err, user) {
+    if(err) return res.status(500).send(err);
+    if(!user) return res.status(404).send({ err: "User not found"});
+    res.send("User find successfully:\n" + user);
+  });
+  
 };
 
 exports.findByIdAndUpdate = function (req, res) {
--- projects/users_api/router/index.js
+++ projects/users_api/router/index.js
@@ -6,8 +6,8 @@ var users_controller = require('../controller/users.controller.js');
 
 router.post("/users", users_controller.create );
 router.get("/users", users_controller.findAll );
-router.get("/users/username/:username", users_controller.findOne );
-router.get("/users/:id", users_controller.findByName ) ;
+router.get("/users/username/:username", users_controller.findByName );
+router.get("/users/:id", users_controller.findOne ) ;
 router.put("/users/:id", users_controller.findByIdAndUpdate );
 router.put("/users/:id", users_controller.findByNameAndUpdate );
 router.put("/users/username/:username", users_controller.findByNameAndUpdate );
