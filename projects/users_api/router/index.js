1edd650a20fa8758e224f4a2f594843829f1e556
minung han <hmu332233@gmail.com>
2017-08-03 20:48:21 +0900

    [수정] router 주석 제거

--- projects/users_api/router/index.js
+++ projects/users_api/router/index.js
@@ -3,31 +3,15 @@ var router = express.Router();
 
 var users_controller = require('../controller/users.controller.js');
 
-// Create
+
 router.post("/users", users_controller.create );
-
-// Find All
 router.get("/users", users_controller.findAll );
-
-// Find One
 router.get("/users/username/:username", users_controller.findOne );
-
-// Find By Name
 router.get("/users/:id", users_controller.findByName ) ;
-
-// GET A USER, THEN UPDATE
 router.put("/users/:id", users_controller.findByIdAndUpdate );
-
-// FIND BY ID AND UPDATE
 router.put("/users/:id", users_controller.findByNameAndUpdate );
-
-// FIND AND UPDATE
 router.put("/users/username/:username", users_controller.findByNameAndUpdate );
-
-// REMOVE ALL
 router.delete("/users/", users_controller.deleteAll );
-
-// FIND AND REMOVE
 router.delete("/users/username/:username", users_controller.delete );
 
 
